0d32a02c89723312eb2d42724d03805c6a92650a
LRonElectricy <LRon006900@gmail.com>
2023-05-27 13:38:54 +0300

    1

--- src/main.js
+++ src/main.js
@@ -4,7 +4,28 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+
+//google maps
 import * as VueGoogleMaps from 'vue2-google-maps'
+
+//firebase
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/database'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBe6eYg7PC3tbVF6YxgBVJ9T3q4MzrlDDc",
+  authDomain: "mythical-pod-582.firebaseapp.com",
+  databaseURL: "https://mythical-pod-582-default-rtdb.firebaseio.com",
+  projectId: "mythical-pod-582",
+  storageBucket: "mythical-pod-582.appspot.com",
+  messagingSenderId: "66874463517",
+  appId: "1:66874463517:web:909a31138e94f38d5b3df4",
+  measurementId: "G-4KXENXP6WT"
+};
+
+var fire = firebase.initializeApp(firebaseConfig);
 
 Vue.use(VueGoogleMaps, {
   load: {
@@ -21,3 +42,5 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+export default fire;
